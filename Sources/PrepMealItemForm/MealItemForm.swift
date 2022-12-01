604c465c4037486d64e9fcd43006c9c74dd2896c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-01 11:29:22 +0500

    bump

--- Sources/PrepMealItemForm/MealItemForm.swift
+++ Sources/PrepMealItemForm/MealItemForm.swift
@@ -308,36 +308,23 @@ public struct MealItemForm: View {
                 date: viewModel.date,
                 showCloseButton: false,
                 selectedGoalSet: viewModel.day?.goalSet
-            ) { tappedGoalSet in
-                do {
-                    
-                    //TODO: Do these
-                    /// [ ] Look into what `forMealItemForm` is used for as it may be redundant
-                    /// [ ] Make `GoalSetsList` create the day and set the diet in one place
-                    /// [ ] Instead of this, get use `didChangeGoalSet(to goalSet: GoalSet?, and day: Day?)`
-                    /// [ ] Include the "+" add button when in goal selection mode too
-                    /// [ ] When in goal selection mode, adding a button should immediatly select it and dismiss, wile returning the callback
-                    /// [ ] If a `Day` was supplied, set that in the `viewModel` first
-                    ///
-                    /// Now reuse this for the meal types picker
-                    /// [ ] Do the same, except we don't care about `Day`, and simply set meal's `goalSet` to what was provided (a `GoalSet` or `nil`)
-                    /// [ ] Create a helper (on `GoalSetForm`) that completes the form with mock data when invoked
-                    
-                    guard let day = viewModel.day else {
-                        return
-                    }
-                    
-                    if day.goalSet?.id == tappedGoalSet.id {
-//                        try DataManager.shared.removeGoalSet(on: day.date)
-                        viewModel.day?.goalSet = nil
-                    } else {
-//                        try DataManager.shared.setGoalSet(tappedGoalSet, on: day.date)
-                        viewModel.day?.goalSet = tappedGoalSet
-                    }
-//                    NotificationCenter.default.post(name: .didUpdateDiet, object: nil)
-                } catch {
-                    print("Error setting GoalSet: \(error)")
+            ) { tappedGoalSet, day in
+                
+                if let day {
+                    viewModel.day = day
                 }
+                
+                //TODO: Do these
+                /// [x] Look into what `forMealItemForm` is used for as it may be redundant
+                /// [x] Make `GoalSetsList` create the day and set the diet in one place
+                /// [x] Instead of this, get use `didChangeGoalSet(to goalSet: GoalSet?, and day: Day?)`
+                /// [x] Include the "+" add button when in goal selection mode too
+                /// [x] When in goal selection mode, adding a button should immediatly select it and dismiss, wile returning the callback
+                /// [x] If a `Day` was supplied, set that in the `viewModel` first
+                ///
+                /// Now reuse this for the meal types picker
+                /// [ ] Do the same, except we don't care about `Day`, and simply set meal's `goalSet` to what was provided (a `GoalSet` or `nil`)
+                /// [ ] Create a helper (on `GoalSetForm`) that completes the form with mock data when invoked
             }
         }
         .presentationDetents([.medium, .large])
